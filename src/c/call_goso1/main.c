7e9fcaf290601b7802e8323476ecb02cb82627d9
root <root@f35.vpnservice.company>
2021-12-05 13:08:18 -0500

    m

--- src/c/call_goso1/main.c
+++ src/c/call_goso1/main.c
@@ -2,7 +2,15 @@
 #include "goso1.h"
 
 int main() {
-    printf("Using awesome lib from C:\n");
-   
+    //Call golang Add_goso1 function
+    GoInt a = 12;
+    GoInt b = 99;
+    fprintf(stderr, "[%d] %s> goso1.Add_goso1(%d,%d) = %d\n", 
+      getpid(), 
+      "call_goso1", 
+      a, b,
+      Add_goso1(a, b)
+    ); 
+  
 }
 
