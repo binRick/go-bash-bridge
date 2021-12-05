32c02e06d9d4eebc6fa603e6f57b28d7a475729e
root <root@f35.vpnservice.company>
2021-12-05 14:02:58 -0500

    m

--- src/c/call_goso1/main_dl.c
+++ src/c/call_goso1/main_dl.c
@@ -1,35 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <dlfcn.h>
-//#include "goso1/_obj/_cgo_export.h"
+///#include "/root/go-bash-bridge/src/go/goso1/_obj/_cgo_export.h"
+//#include "/root/go-bash-bridge/src/go/goso1/_obj/_cgo_export.c"
+//#include "/root/go-bash-bridge/src/go/goso1/_obj/main.cgo2.c"
+//#include "/root/go-bash-bridge/src/go/goso1/_obj/_cgo_main.c"
+#include "/root/go-bash-bridge/RELEASE/include/goso1.h"
 //#include "goso1/_obj/_cgo_export.c"
 //#include "goso1/_obj/main.cgo2.c"
 
-//typedef long long go_int;
+typedef long long go_int;
 
 
 int main() {
     void *handle;
     char *error;
 
-    handle = dlopen ("./../lib/goso1.so", RTLD_LAZY);
+    handle = dlopen ("/root/go-bash-bridge/RELEASE/lib/goso1.so", RTLD_LAZY);
     if (!handle) {
         fputs (dlerror(), stderr);
         exit(1);
     }
-    int a = 12;
-    int b = 99;
-/*
-    //Call golang Add_goso1 function
-    GoInt a = 12;
-    GoInt b = 99;
+    go_int a = 12;
+    go_int b = 98;
     fprintf(stderr, "[%d] %s> goso1.Add_goso1(%d,%d) = %d\n", 
       getpid(), 
       "call_goso1", 
       a, b,
       Add_goso1(a, b)
     ); 
-*/
 /*
 */
     dlclose(handle);
