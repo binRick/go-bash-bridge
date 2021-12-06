b45fdb21b38f47c879f6972a309bf9b4bfd84bd5
Richard Blundell <rblundell@deloitte.com>
2021-12-06 13:27:55 -0500

    clean up logic

--- scripts/mkdiff.sh
+++ scripts/mkdiff.sh
@@ -1,5 +1,6 @@
 diff -Naur eval.c.orig eval.c > ../../../patches/010-eval-command-callback.patch
 diff -Naur builtins/cd.def.orig builtins/cd.def > ../../../patches/011-builtins-cd.patch
+diff -Naur execute_cmd.c.orig execute_cmd.c > ../../../patches/012-execute-cmd.c.patch
 
 #bat ../../../patches/010-eval-command-callback.patch
 git add ../../../patches/*.patch
